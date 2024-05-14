5f043ab6b2b76dccc7c8cdb986f6d1293828cf02
Enzo0708 <enzobiason7@gmail.com>
2024-05-14 11:09:05 -0300

    up

--- Aula_teste/hotel/views.py
+++ Aula_teste/hotel/views.py
@@ -7,3 +7,9 @@ def homepage(request):
     dados_hotel = hotel.objects.all()
     context["dados_hotel"] = dados_hotel
     return render(request, 'homepage.html', context)
+
+def page_quartos(request):
+    context = {}
+    dados_quartos = quarto.objects.all()
+    context["dados_quartos"] = dados_quartos
+    return render(request, 'quartos.html', context)
